826c0a2d467752b7c67f504cf3b5507984d235a9
ToPhuocNguyen <nguyentp@pcsoftvn.com>
2017-05-11 15:28:41 -0700

    nothing

--- 2.InlineAnonymousListener/gen/com/ToPhuocNguyen/inlineanonymouslistener/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.ToPhuocNguyen.inlineanonymouslistener;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- baiTap11SuDungCheckboxVaRadioButtonToPhuocNguyen/gen/com/example/baitap11sudungcheckboxvaradiobuttontophuocnguyen/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.example.baitap11sudungcheckboxvaradiobuttontophuocnguyen;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
