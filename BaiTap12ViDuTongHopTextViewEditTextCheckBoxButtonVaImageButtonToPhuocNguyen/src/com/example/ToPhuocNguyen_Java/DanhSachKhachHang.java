0e1ece1eb09e44d8f97f1f2fc42edfe87c9bd788
ToPhuocNguyen <nguyentp@pcsoftvn.com>
2017-05-11 09:14:00 -0700

    BaiTap12ViDuTongHopTextViewEditTextCheckBoxButtonVaImageButton

---
+++ BaiTap12ViDuTongHopTextViewEditTextCheckBoxButtonVaImageButtonToPhuocNguyen/src/com/example/ToPhuocNguyen_Java/DanhSachKhachHang.java
@@ -0,0 +1,33 @@
+package com.example.ToPhuocNguyen_Java;
+import java.util.ArrayList;
+
+public class DanhSachKhachHang {
+	ArrayList<KhachHang>listKH=new ArrayList<KhachHang>();
+	public void addKhachHang(KhachHang kh)
+	{
+		listKH.add(kh);
+	}
+	public double tongDoanhThu()
+	{
+		double tien=0.0;
+		for(KhachHang kh:listKH)
+		{
+			tien+=kh.tinhThanhTien();
+		}
+		return tien;
+	}
+	public int tongKhachHang()
+	{
+		return listKH.size();
+	}
+	public int tongKhachHangVip()
+	{
+		int s=0;
+		for(KhachHang kh:listKH)
+		{
+			if(kh.isVip())
+				s++;
+		}
+		return s;
+	}
+}
